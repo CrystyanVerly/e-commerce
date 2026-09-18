af98301abc1c91d7f46433325c490164dd77e5d5
unknown <crystyanknappverly@gmail.com>
2026-09-18 12:01:51 -0300

    feat(header): add provisional header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import styles from './Header.module.css';
+import { Container } from '../../../layouts/Container/Container';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-	return <div></div>;
+	return (
+		<header className={styles.header}>
+			<Container className={styles.container}>
+				<Link to="/" className={styles.logo}>
+					Ecommerce
+				</Link>
+
+				<nav aria-label="Main navigation">
+					<ul className={styles.navigation}>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="shop">Shop</Link>
+						</li>
+					</ul>
+				</nav>
+			</Container>
+		</header>
+	);
 };
 
 export default Header;
