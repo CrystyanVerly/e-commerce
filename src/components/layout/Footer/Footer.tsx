35ebdc6ae5d7ca061e684664d1c58ffbe116ba24
unknown <crystyanknappverly@gmail.com>
2026-09-18 12:08:42 -0300

    feat(component): add provisional footer

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import { Container } from '../../../layouts/Container/Container';
+import styles from './Footer.module.css';
 
 const Footer = () => {
-	return <div></div>;
+	return (
+		<footer className={styles.footer}>
+			<Container>
+				<p>© Ecommerce | Some rights reserved</p>
+			</Container>
+		</footer>
+	);
 };
 
 export default Footer;
